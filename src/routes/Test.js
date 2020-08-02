97e2ef51ec4a9dcf176a8bf20ab12406c937fe3f
seungju <kingsky32@gmail.com>
2020-08-02 12:48:55 +0900

    Edit Problems key, idx

--- src/routes/Test.js
+++ src/routes/Test.js
@@ -5,47 +5,38 @@ import Problem from "../components/Problem";
 
 const problems = [
   {
-    idx: 0,
     title: "1번문제 타이틀",
     test: [{ text: "문항 1번" }, { text: "문항 2번" }]
   },
   {
-    idx: 1,
     title: "2번문제 타이틀",
     test: [{ text: "문항 1번" }, { text: "문항 2번" }]
   },
   {
-    idx: 2,
     title: "3번문제 타이틀",
     test: [{ text: "문항 1번" }, { text: "문항 2번" }]
   },
   {
-    idx: 3,
     title: "4번문제 타이틀",
     test: [{ text: "문항 1번" }, { text: "문항 2번" }]
   },
   {
-    idx: 4,
     title: "5번문제 타이틀",
     test: [{ text: "문항 1번" }, { text: "문항 2번" }]
   },
   {
-    idx: 5,
     title: "6번문제 타이틀",
     test: [{ text: "문항 1번" }, { text: "문항 2번" }]
   },
   {
-    idx: 6,
     title: "7번문제 타이틀",
     test: [{ text: "문항 1번" }, { text: "문항 2번" }]
   },
   {
-    idx: 7,
     title: "8번문제 타이틀",
     test: [{ text: "문항 1번" }, { text: "문항 2번" }]
   },
   {
-    idx: 8,
     title: "9번문제 타이틀",
     test: [{ text: "문항 1번" }, { text: "문항 2번" }]
   }
@@ -94,8 +85,8 @@ const Test = () => {
     <Wrapper>
       <ProblemWrapper>
         <ProblemContainer>
-          {problems.map(problem =>
-            <Problem key={problem.idx} {...problem} nowPage={nowPage} onClick={onClick} />
+          {problems.map((problem, idx) =>
+            <Problem key={idx} idx={idx} {...problem} nowPage={nowPage} onClick={onClick} />
           )}
         </ProblemContainer>
       </ProblemWrapper>
